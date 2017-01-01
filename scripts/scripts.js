b3f111c4462a1584976b01e87ca80d6a6f6559e4
Eric Shell <ericjshell@gmail.com>
2017-01-01 15:17:24 -0500

    Adding basic slider functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,25 @@
 (function ($) {
+	$(document).ready(function() {
 
+		$('#clients ul').slick({
+      prevArrow: '<svg class="slick-prev" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path class="path" d="M10.2 86.9l13.3-13.3 126.7 126.6L276.9 73.6l13.3 13.3-140 140-140-140z"/></svg>',
+      nextArrow: '<svg class="slick-next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path class="path" d="M10.2 86.9l13.3-13.3 126.7 126.6L276.9 73.6l13.3 13.3-140 140-140-140z"/></svg>',
+      dots: true,
+      arrows: true,
+			slidesToShow: 4,
+  		slidesToScroll: 4,
+      accessibility: false,
+      responsive: [
+        {
+          breakpoint: 1000,
+          settings: {
+            arrows: false,
+            slidesToShow: 2,
+  					slidesToScroll: 2,
+          }
+        },
+      ]
+    });
+
+	});
 })(jQuery);
