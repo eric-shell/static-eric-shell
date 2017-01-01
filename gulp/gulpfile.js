f12b19ff789fc7a94c980cee4595be84138b745b
Eric Shell <ericjshell@gmail.com>
2017-01-01 15:44:40 -0500

    Creating concatenated and minified files for all CSS and all JS files

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,22 +5,33 @@
 
 var gulp = require('gulp')
     sass = require('gulp-sass')
+    clean = require('gulp-clean-css')
     rename = require('gulp-rename')
-    minify = require('gulp-minify-css')
+    concat = require('gulp-concat')
+    uglify = require('gulp-uglify')
     prefixer = require('gulp-autoprefixer')
     sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
   gulp.src('../styles/source.scss')
     .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(prefixer())
-      .pipe(minify())
-      .pipe(rename('styles.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefixer())
+    .pipe(clean())
+    .pipe(rename('styles.min.css'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('../styles'));
+    .pipe(gulp.dest('../min'))
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('js', function () {
+  gulp.src(['../scripts/vendor/*.js', '../scripts/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('../min'));
+});
+
+gulp.task('default', ['js', 'sass'], function() {
+  gulp.watch('../scripts/**/*.js', ['js']);
   gulp.watch('../styles/**/*.scss', ['sass']);
 });
