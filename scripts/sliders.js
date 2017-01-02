589948107de851a77d82f202f28bd068485e2df1
Eric Shell <ericjshell@gmail.com>
2017-01-01 19:44:14 -0500

    Porting custom resume interaction over

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -21,5 +21,35 @@
       ]
     });
 
+    // Resume page
+    // When the resume link for each panel is clicked
+    $('a.next-panel').click(function(e){
+
+      // Start the resume experience
+      if($(this).hasClass('start')){
+        event.preventDefault(e);
+        $(this).parent().parent().removeClass('visible');
+        $(this).parent().parent().next('.panel').addClass('visible');
+      }
+
+      // Show next the panel
+      else{
+        event.preventDefault(e);
+        $(this).parent().parent().next('.panel').addClass('visible');
+        $('html,body').animate({
+          scrollTop:$(this).parent().parent().next().offset().top
+        }, 500);
+      }
+    });
+
+    // Create sliders
+    $('.slider').slick({
+      prevArrow: '<svg class="slick-prev" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path class="path" d="M10.2 86.9l13.3-13.3 126.7 126.6L276.9 73.6l13.3 13.3-140 140-140-140z"/></svg>',
+      nextArrow: '<svg class="slick-next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path class="path" d="M10.2 86.9l13.3-13.3 126.7 126.6L276.9 73.6l13.3 13.3-140 140-140-140z"/></svg>',
+      dots: true,
+      infinite: true,
+      adaptiveHeight: true
+    });
+
 	});
 })(jQuery);
