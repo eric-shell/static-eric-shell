4c54fb8efeb59a4df3863e507e28475ad0066d83
Eric Shell <ericjshell@gmail.com>
2017-02-08 17:29:18 -0800

    Adjusting client functionality and slider default theming

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -5,7 +5,7 @@
       prevArrow: '<svg class="slick-prev" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path class="path" d="M10.2 86.9l13.3-13.3 126.7 126.6L276.9 73.6l13.3 13.3-140 140-140-140z"/></svg>',
       nextArrow: '<svg class="slick-next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path class="path" d="M10.2 86.9l13.3-13.3 126.7 126.6L276.9 73.6l13.3 13.3-140 140-140-140z"/></svg>',
       dots: true,
-      arrows: true,
+      arrows: false,
       lazyLoad: 'ondemand',
 			slidesToShow: 4,
   		slidesToScroll: 4,
@@ -14,7 +14,6 @@
         {
           breakpoint: 1000,
           settings: {
-            arrows: false,
             slidesToShow: 2,
   					slidesToScroll: 2,
           }
